d330e7220ced006210b4202b4e361f27fe1eb2d4
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-07-02 16:51:43 -0500

    Added in some comments to my solution for the bonus question to practice documenting my code

--- js/planets-string.js
+++ js/planets-string.js
@@ -23,17 +23,23 @@
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+    // join planetsArray with break html tags as delimiter
     console.log(planetsArray.join("<br>"));
 
-    //
+    // Declared an empty array to build new array of planets with opening and closing list item tags
     let planetsArr = [];
     for(let i = 0; i < planetsArray.length; i++) {
         planetsArr.push("<li>");
         planetsArr.push(planetsArray[i]);
         planetsArr.push("</li>");
     }
+
+    // Add opening and closing unordered list tags to the newly built planets array (planetsArr)
     planetsArr.unshift("<ul>");
     planetsArr.push("</ul>");
-    console.log(planetsArr.join(""));
-    // document.getElementById("planetsUL").innerHTML = planetsArr.join("");
+
+    // join planetsArr and store in strList variable
+    const strList = planetsArr.join("");
+    console.log(strList);
+    document.getElementById("planetsUL").innerHTML = strList;
 })();
