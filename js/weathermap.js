c01dfef1d1f0c32345d5796bbc15389e61d34335
Jesus Rogelio Garza <jesusgarza2511@gmail.com>
2020-07-24 20:44:57 -0500

    Got the for loop working for the 5 weather cards (5 day forecasts info) and got the mapbox onto the page. Now the markers need to be made for the map, and also need to figure out how to get the map marker information from mapbox to the openWeather api to get the expected marker's (the city the marker is within) weather forecast information.

--- js/weathermap.js
+++ js/weathermap.js
@@ -1,31 +1,57 @@
 // All of the code used to make AJAX requests to both OpenWeatherMap and Mapbox APIs
 
-$.get("http://api.openweathermap.org/data/2.5/weather", {
+$.get(`https://api.openweathermap.org/data/2.5/onecall`, {
     APPID: OPEN_WEATHER_APPID,
-    q:     "San Antonio, US",
+    lat: 29.419,
+    lon: -98.492,
     units: "imperial"
 }).done(function(response) {
-    console.log(response);
 
-    // Assign the pretty Date String to #weather-date
-    let date = new Date(response.dt * 1000);
+    // loop through 5 days of weather forecast
+    for(let i = 0; i < 5; i++) {
+        // build specific card id to target DOM element
+        const cardId = `card-${i}`;
 
-    $("#weather-date").html(date.toDateString());
-    console.log(date.toDateString());
+        // Assign variables to the attributes of the response object
+        /**
+         * Assign card values:
+         * Weather Date
+         * Low/High Temperatures
+         * feels_like
+         * humidity
+         * pressure
+         * */
+        // Assign the pretty Date String to #weather-date
+        const date = new Date(response.daily[i].dt * 1000);
+        const icon = `http://openweathermap.org/img/wn/${response.current.weather[0].icon}@2x.png`;
+        const lowTemp = response.daily[i].temp.min;
+        const highTemp = response.daily[i].temp.max;
+        const feelsLike = response.daily[i].feels_like.day;
+        const humidity = response.daily[i].humidity;
+        const pressure = response.daily[i].pressure;
 
-    // Assign the High/Low temperature to
-    const lowTemp = response.main.temp_min;
-    const highTemp = response.main.temp_max;
-    $("#high-low-temp").html(`<span class="text-primary">${lowTemp}℉</span> / <span class="text-danger">${highTemp}℉</span>`);
-
-    // feels_like: 91.69
-    console.log("Feels Like: " + response.main.feels_like);
-    // humidity: 49
-    console.log("Humidity: " + response.main.humidity);
-    // pressure: 1014
-    console.log("Pressure: " + response.main.pressure);
-    // temp: 91.89
-    console.log("Humidity: " + response.main.humidity);
-    // temp_max: 93
-    // temp_min: 90
+        $(`${cardId}, #weather-date-${i}`).html(`${date.toDateString()}`);
+        $(`${cardId}, #high-low-temp-${i}`)
+            .html(`<div class="row"><div class="col-12"><span class="text-primary">${lowTemp}℉</span> /` +
+                ` <span class="text-danger">${highTemp}℉</span></div>` +
+                `<div class="col-12"><img src=${icon}></div></div>`
+            );
+        $(`${cardId}, #feels-like-${i}`).html(`Feels_Like: ${feelsLike}`);
+        $(`${cardId}, #humidity-${i}`).html(`Humidity: ${humidity}`);
+        $(`${cardId}, #pressure-${i}`).html(`Pressure: ${pressure}`);
+    }
 });
+
+const mapBox = $('#mapBox').css({
+    height: "50em"
+});
+
+mapboxgl.accessToken = MAPBOX_KEY;
+var map = new mapboxgl.Map({
+    container: `mapBox`,
+    style: 'mapbox://styles/mapbox/dark-v10', // stylesheet location
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+// disable map zoom when using scroll
+map.scrollZoom.disable();
